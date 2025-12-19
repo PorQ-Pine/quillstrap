47c6d6f539570ce844ccb8804ccd8b0aa2df25ad
Szybet <53944559+Szybet@users.noreply.github.com>
2025-12-19 19:43:55 +0000

    fix

--- quillstrap/src/things/init/boot_partition.rs
+++ quillstrap/src/things/init/boot_partition.rs
@@ -18,7 +18,7 @@ impl SetupThing for BootPartition {
 
     fn deps(&self) -> Vec<&'static str> {
         // It needs them to deploy, but makes sense to keep them here
-        vec!["kernel", "eink_kernel_magic", "firmware"]
+        vec!["kernel", "eink_kernel_magic", "firmware", "procedural_wallpapers"]
     }
 
     fn git(&self) -> &'static str {
